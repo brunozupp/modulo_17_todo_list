59b399c3cc27d5c61e0c5ee260c6d151e5dfa62a
bruno.noveli <brunimzuppwolf@gmail.com>
2022-04-22 14:35:14 -0300

    Construindo Layout da HomePage - Parte 1

--- lib/app/modules/home/home_page.dart
+++ lib/app/modules/home/home_page.dart
@@ -1,5 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:modulo_17_todo_list/app/core/auth/auth_provider.dart';
+import 'package:modulo_17_todo_list/app/core/ui/theme_extensions.dart';
+import 'package:modulo_17_todo_list/app/core/ui/todo_list_icons.dart';
+import 'package:modulo_17_todo_list/app/modules/home/widgets/home_filters.dart';
+import 'package:modulo_17_todo_list/app/modules/home/widgets/home_header.dart';
 import 'package:provider/provider.dart';
 
 class HomePage extends StatelessWidget {
@@ -10,16 +14,53 @@ class HomePage extends StatelessWidget {
    Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Home Page'),
-      ),
-      drawer: const HomePage(),
-      body: Center(
-        child: TextButton(
-          onPressed: () {
-            context.read<AuthProvider>().logout();
-          }, 
-          child: Text("Logout"),
+        iconTheme: IconThemeData(
+          color: context.primaryColor,
         ),
+        backgroundColor: Colors.transparent,
+        elevation: 0,
+        actions: [
+          PopupMenuButton(
+            icon: const Icon(
+              TodoListIcons.filter,
+            ),
+            itemBuilder: (_) => <PopupMenuItem<bool>>[
+              const PopupMenuItem<bool>(
+                child: Text(
+                  "Mostrar tarefas concluidas",
+                ),
+              ),
+            ],
+          ),
+        ],
+      ),
+      backgroundColor: const Color(0xFFFAFBFE),
+      drawer: const HomePage(),
+      body: LayoutBuilder(
+        builder: (context, constraints) {
+          return SingleChildScrollView(
+            child: ConstrainedBox(
+              constraints: BoxConstraints(
+                minHeight: constraints.maxHeight,
+                minWidth: constraints.maxWidth,
+              ),
+              child: Container(
+                margin: EdgeInsets.symmetric(
+                  horizontal: 20,
+                ),
+                child: IntrinsicHeight(
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      HomeHeader(),
+                      HomeFilters(),
+                    ],
+                  ),
+                ),
+              ),
+            ),
+          );
+        },
       ),
     );
   }
---
+++ lib/app/modules/home/widgets/home_header.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+import 'package:modulo_17_todo_list/app/core/auth/auth_provider.dart';
+import 'package:modulo_17_todo_list/app/core/ui/theme_extensions.dart';
+import 'package:provider/provider.dart';
+
+class HomeHeader extends StatelessWidget {
+  const HomeHeader({ Key? key }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      children: [
+        Padding(
+          padding: const EdgeInsets.symmetric(vertical: 20),
+          child: Selector<AuthProvider,String>(
+            selector: (context, authProvider) {
+              return authProvider.user?.displayName ?? "Não informado";
+            },
+            builder: (context,displayName,child) {
+              return Text(
+                "E ai, $displayName!",
+                style: context.textTheme.headline5?.copyWith(
+                  fontWeight: FontWeight.bold,
+                ),
+              );
+            }
+          ),
+        ),
+      ],
+    );
+  }
+}
